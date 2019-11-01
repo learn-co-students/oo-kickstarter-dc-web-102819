623b1d3bc07a682fe4f7ab3d02ae1b9bce7ffc68
singingcactus <bechma02@gmail.com>
2019-11-01 12:17:41 -0400

    added code

---
+++ lib/project.rb
@@ -0,0 +1,19 @@
+class Project
+
+    attr_accessor :title
+    attr_reader :backers
+
+    def initialize(project_name)
+        @title = project_name
+        @backers = []
+    end
+
+    def add_backer(backer)
+        unless self.backers.include?(backer)
+         self.backers << backer
+            backer.back_project(self)
+        end
+    end
+
+
+end
