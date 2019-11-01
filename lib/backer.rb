b2e5c9034c9f9cfcf62ee0a634a247a85d2738ad
Flatiron School <stvik2@gmail.com>
2019-11-01 17:21:18 -0400

    draft3

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,6 +1,6 @@
-require ‘pry’
-require_relative backed_projects.rb
-require_relative project.rb
+require "pry"
+require_relative "backed_projects.rb"
+require_relative "project.rb"
 
 class Backer
   # Backer will no longer need to have the Project Name
@@ -9,16 +9,19 @@ class Backer
      @name = name
    end
    def back_project(project)
-       new.Backer_Projects(self , project)
+       Backer_Projects.new(self , project)
    end
    def fetch_projects
-     myprojects = Backer_Projects.all.select do |backed_projects|
+     myprojects = Backer_Projects.all.select do |backed_project|
+       backed_project.backer == self
        binding.pry
-       backed_projects.backer == self
      end
+
    end
 end
-sara = new.Backer(“Sara”)
-project_name = new.Project(“Save the trees”)
+sara = Backer.new("Sara")
+project_name = Project.new("Save the trees")
 sara.back_project(project_name)
+binding.pry
 # backed_projects = new.Backed_Projects(“Sara” , “Save The Trees”) ;
+
