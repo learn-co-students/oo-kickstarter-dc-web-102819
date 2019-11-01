8c8b7fd2a5c5d96f3b082f9de848e8845503edab
Flatiron School <stvik2@gmail.com>
2019-11-01 16:30:34 -0400

    draft2

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,17 +1,24 @@
+require ‘pry’
+require_relative backed_projects.rb
+require_relative project.rb
+
 class Backer
-
-    attr_accessor :backed_projects
-    attr_reader :name
-    #initialize Backer with name and empty backed_projects array
-    def initialize(name)
-        @name = name
-        @backed_projects = []
-    end
-    #'backs' a project by storing it into backed_projects array. Uses method add_backer in 
-    #project class to add self to @backers array
-    def back_project(project)
-        @backed_projects << project
-        project.backers << self
-    end
-
+  # Backer will no longer need to have the Project Name
+   attr_reader :name
+   def initialize(name)
+     @name = name
+   end
+   def back_project(project)
+       new.Backer_Projects(self , project)
+   end
+   def fetch_projects
+     myprojects = Backer_Projects.all.select do |backed_projects|
+       binding.pry
+       backed_projects.backer == self
+     end
+   end
 end
+sara = new.Backer(“Sara”)
+project_name = new.Project(“Save the trees”)
+sara.back_project(project_name)
+# backed_projects = new.Backed_Projects(“Sara” , “Save The Trees”) ;
